8a4664e6f5115430843adacf0a4d04cc366a17f6
LilMark0o <marcoalejandroramirezc@gmail.com>
2025-03-31 22:29:45 -0500

    exe file + documentation

--- cube_raytracer.cpp
+++ cube_raytracer.cpp
@@ -32,9 +32,10 @@ public:
     Vec3() : e{0, 0, 0} {}
     Vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-    double x() const { return e[0]; }
-    double y() const { return e[1]; }
-    double z() const { return e[2]; }
+    // Coordinate accessors
+    inline double x() const { return e[0]; }
+    inline double y() const { return e[1]; }
+    inline double z() const { return e[2]; }
 
     Vec3 operator-() const { return Vec3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
